648a73a9036fdead6bf153a954423c020062a5e2
Bindu K Reddy <bindukreddy1111@gmail.com>
2026-09-03 13:50:01 +0530

    Connect frontend to deployed backend

--- src/components/AIAssistant.jsx
+++ src/components/AIAssistant.jsx
@@ -27,7 +27,7 @@ function AIAssistant() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/chat", {
+      const response = await fetch("https://wander-backend-z7kf.onrender.com/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
